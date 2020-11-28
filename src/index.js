c6f31bf6cf3e880ff6d26d8b3be14f17ea046a65
Savchhenko <savchenko@phys.vsu.ru>
2020-11-28 22:48:14 +0300

    creating React's element

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// создадим react-элемент:
+// 1ый аргумент - элемент, который хотим отобразить
+// 2ой аргумент - свойства этого h1 элемента (его данные/props)
+// 3ий аргумент - значение элемента
+
+// const el = React.createElement(
+//   'h1', 
+//   null, 
+//   'Hello World, React.js!',
+// );
+
+const el = <h1>Hello World, React.js!</h1> // JSX 
+//JSX компилируется babel'ем в скрипт, который приведен выше
+
+//покажем этот элемент на странице с помощью библиотеки ReactDOM:
+// 1ый аргумент - это сам элемент, который хотим показать
+// 2ой аргумент - место, где хотим его отобразить
+
+ReactDOM.render(el, document.getElementById('root'));
 
